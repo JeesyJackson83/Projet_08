f1121c5133898800caffd3056ef90bc74b8f9a89
JessyJackson83 <jessyjackson83@gmail.com>
2019-11-14 03:38:10 +0100

    add some urls test

--- purbeurre/purbeurre/tests/test_urls.py
+++ purbeurre/purbeurre/tests/test_urls.py
@@ -1,6 +1,9 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from homepage.views import home, mentions
+from users.views import register, profile
+from aliments_off.views import SearchView, ResultView, DetailProductView, SaveView, MyProductsView
+
 
 class TestUrls(SimpleTestCase):
 
@@ -11,3 +14,35 @@ class TestUrls(SimpleTestCase):
     def test_mentions_url_resolves(self):
         url = reverse('mentions')
         self.assertEquals(resolve(url).func, mentions)
+
+    def test_register_url_resolves(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func, register)
+
+    def test_profile_url_resolves(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func, profile)
+
+    # def test_login_url_resolves(self):
+    #     url = reverse('login')
+    #     self.assertEquals(resolve(url).func, login)
+
+    # def test_logout_url_resolves(self):
+    #     url = reverse('logout')
+    #     self.assertEquals(resolve(url).func, logout)
+
+    def test_search_url_resolves(self):
+        url = reverse('aliments_off:search')
+        self.assertEquals(resolve(url).func.view_class, SearchView)
+
+    # def test_search_url_resolves(self):
+    #     url = reverse('aliments_off:result')
+    #     self.assertEquals(resolve(url).func.view_class, ResultView)
+
+    def test_save_url_resolves(self):
+        url = reverse('aliments_off:save')
+        self.assertEquals(resolve(url).func, SaveView)
+
+    def test_myproducts_url_resolves(self):
+        url = reverse('aliments_off:myproducts')
+        self.assertEquals(resolve(url).func.view_class, MyProductsView)
