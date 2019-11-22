0583d098dc9bb4149821be428dc5bc3400d18669
JessyJackson83 <jessyjackson83@gmail.com>
2019-11-22 04:07:27 +0100

    settings upt 2

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'purbeurre'
+    'purbeurre',
     'homepage',
     'aliments_off',
     'users',
