b43ccb7882d284e80368c8caf5a20c6128113b83
JessyJackson83 <jessyjackson83@gmail.com>
2019-11-24 02:40:59 +0100

    settings minor upt

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -94,9 +94,6 @@ DATABASES = {
     }
 }
 
-db_from_env = django_heroku.dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
