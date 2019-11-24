d9f09474b00af9d208ac31bcbcab7721d687fb82
JessyJackson83 <jessyjackson83@gmail.com>
2019-11-24 05:04:09 +0100

    test raté back to clean

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -133,22 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    )
-else:
-    FORCE_SCRIPT_NAME = '/purbeurre'
-    STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
-    STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
-    )
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
