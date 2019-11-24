cbc2006cad9a38f3cc03ed293a9af2191e023014
JessyJackson83 <jessyjackson83@gmail.com>
2019-11-24 04:44:36 +0100

    add condition to settings

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -132,8 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if DEBUG:
+    STATIC_URL = '/static/'
+else:
+    FORCE_SCRIPT_NAME = '/purbeurre'
+    STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
