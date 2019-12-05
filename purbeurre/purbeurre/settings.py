2b921fc15b86cd96293c5cf059bd9be44856e27c
JessyJackson83 <jessyjackson83@gmail.com>
2019-12-05 20:43:12 +0100

    change ALLW HOST

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['jjpurbeurre.herokuapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['172.105.65.47']
 
 
 # Application definition
